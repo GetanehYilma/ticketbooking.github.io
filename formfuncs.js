87380e1b9ccc7befadac23e68d1b23b6145d2e47
GetanehYilma <gyletike@mum.edu>
2019-09-13 13:20:11 -0500

    second commit

--- formfuncs.js
+++ formfuncs.js
@@ -2,25 +2,30 @@ $(function () {
     // alert('Hello World From JQuery.');
     $("input").addClass("makeBlock");
     $("#booking").on('submit', validateForm);
+    // $('#booking').submit(validateForm());
 
     function validateForm(event) {
-        if ($("#fname").val() == "" || $("#name".val().length <= 0)) {
+        // event.preventDefault();
+        // $('#errors').empty();
+
+
+        if ($("#fname").val() === "" || $("#name".val().length <= 0)) {
             $("#forError").prepend($("<p>", {
                 "class": "error", "text": "Name cannot be empty!"
             }));
-            event.preventDefault();
+
         }
-        else if ($("#zip").val().length !== 5){
+        else if ($('#zip').val().length !== 5){
                 $("#forError").prepend($("<p>", {
                     "class" : "error", "text" : "Enter a 5 digit zip code!"
                 }));
-            event.preventDefault();
             }
-        else if ($("#seat").val() == ""){
-            $("#forError").prepend($("<p>", {
+        else if ($('input[name="seatNumber"]').val() <= 0){
+            $('#forError').prepend($('<p>', {
                 "class" : "error", "text" : "Enter number of seats!"
             }));
             event.preventDefault();
+
         }
 
     }
