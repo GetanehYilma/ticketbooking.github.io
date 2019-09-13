3be236f8aff4f93efedf2c29594d6ffcb9d55bc8
GetanehYilma <gyletike@mum.edu>
2019-09-12 23:59:47 -0500

    first commit

--- formfuncs.js
+++ formfuncs.js
@@ -4,9 +4,21 @@ $(function () {
     $("#booking").on('submit', validateForm);
 
     function validateForm(event) {
-        if ($("#name").val() == "" || $("#name".val().length <= 0)){
+        if ($("#fname").val() == "" || $("#name".val().length <= 0)) {
             $("#forError").prepend($("<p>", {
-                "class" : "error", "text" : "Name cannot be empty!"
+                "class": "error", "text": "Name cannot be empty!"
+            }));
+            event.preventDefault();
+        }
+        else if ($("#zip").val().length !== 5){
+                $("#forError").prepend($("<p>", {
+                    "class" : "error", "text" : "Enter a 5 digit zip code!"
+                }));
+            event.preventDefault();
+            }
+        else if ($("#seat").val() == ""){
+            $("#forError").prepend($("<p>", {
+                "class" : "error", "text" : "Enter number of seats!"
             }));
             event.preventDefault();
         }
