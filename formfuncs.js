175bdb6c3d60000236933fc0a0b117178404404a
GetanehYilma <gyletike@mum.edu>
2019-09-12 22:57:52 -0500

    first commit

--- formfuncs.js
+++ formfuncs.js
@@ -1,5 +1,16 @@
 $(function () {
     // alert('Hello World From JQuery.');
     $("input").addClass("makeBlock");
+    $("#booking").on('submit', validateForm);
+
+    function validateForm(event) {
+        if ($("#name").val() == "" || $("#name".val().length <= 0)){
+            $("#forError").prepend($("<p>", {
+                "class" : "error", "text" : "Name cannot be empty!"
+            }));
+            event.preventDefault();
+        }
+
+    }
 
 });
